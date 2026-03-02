64eb0f996d419204de71dbc68536b2e961fcf37c
cedrickbyrd <cedrickjbyrd@me.com>
2026-03-02 11:11:03 -0600

    Define stripe_public_key variable
    Add variable for Stripe public key integration

--- landing-zone/variables.tf
+++ landing-zone/variables.tf
@@ -151,7 +151,10 @@ variable "lambda_packages" {
   type        = map(string)
 }
 
-
+variable "stripe_public_key" {
+  description = "The public key for Stripe integration"
+  type        = string
+}
 
 
 
