5636a40d9566a0e1c60ae91f6af28399384dad2a
cedrickbyrd <cedrickjbyrd@me.com>
2026-03-02 11:36:11 -0600

    Update path for cloudwatch_dashboard.json file

--- landing-zone/environments/dev/sre_dashboard.tf
+++ landing-zone/environments/dev/sre_dashboard.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_dashboard" "sre_dashboard" {
   dashboard_name = "sre-operations-dashboard"
-  dashboard_body = file("${path.module}/cloudwatch_dashboard.json")
+  dashboard_body = file("${path.module}/../../cloudwatch_dashboard.json")
 }
