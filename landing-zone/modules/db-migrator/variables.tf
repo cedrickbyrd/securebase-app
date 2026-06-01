39747c40d0540247ec7c11621eff37e61c9757aa
cedrickbyrd <cedrickjbyrd@me.com>
2026-06-01 14:21:44 -0500

    fix: expand single-line HCL blocks in db-migrator variables.tf for Terraform 1.5 compat

--- landing-zone/modules/db-migrator/variables.tf
+++ landing-zone/modules/db-migrator/variables.tf
@@ -1,7 +1,30 @@
-variable "environment" { type = string }
-variable "vpc_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "zip_path" { type = string, default = "../../../files/phase6/db_migrator.zip" }
-variable "allowed_secret_arns" { type = list(string) }
-variable "invoker_role_arns" { type = list(string), default = [] }
-variable "tags" { type = map(string), default = {} }
+variable "environment" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "zip_path" {
+  type    = string
+  default = "../../../files/phase6/db_migrator.zip"
+}
+
+variable "allowed_secret_arns" {
+  type = list(string)
+}
+
+variable "invoker_role_arns" {
+  type    = list(string)
+  default = []
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
